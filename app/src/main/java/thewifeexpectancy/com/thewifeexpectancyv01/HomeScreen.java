71adba494d732588a8b3e27672893e5db5644b3a
James Yancey <james.alexander.yancey@gmail.com>
2016-09-08 17:45:23 -0600

    Initial work on the HomeScreen. Having trouble getting things to display correctly on the toolbar

--- app/src/main/java/thewifeexpectancy/com/thewifeexpectancyv01/HomeScreen.java
+++ app/src/main/java/thewifeexpectancy/com/thewifeexpectancyv01/HomeScreen.java
@@ -1,11 +1,10 @@
 package thewifeexpectancy.com.thewifeexpectancyv01;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class HomeScreen extends AppCompatActivity
 {
@@ -17,9 +16,12 @@ public class HomeScreen extends AppCompatActivity
 
         Toolbar my_toolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(my_toolbar);
+        my_toolbar.setTitle("@string/my_tb_title");
 
+        /*
         getSupportActionBar().setTitle(R.string.my_tb_title);
         getSupportActionBar().setIcon(R.drawable.ic_edit);
+
 
         TextView myEntries = (TextView) findViewById(R.id.myEntriesTv);
         myEntries.setOnClickListener(new View.OnClickListener()
@@ -37,6 +39,22 @@ public class HomeScreen extends AppCompatActivity
             public void onClick(View view) {
                 startActivity(new Intent(HomeScreen.this, Specs.class));
             }
-        });
+        });*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.miEdit)
+            return true;
+        else
+            return super.onOptionsItemSelected(item);
     }
 }
